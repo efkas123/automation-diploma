c5289e6e7fb356b779c94b9895ae308817a8dbe8
Filipp Kotov <contact@filippkotov.ru>
2026-04-13 12:36:16 +0300

    Commented currently unused code.

--- src/test/java/api/specs/authorization/PostBringAuthSpec.java
+++ src/test/java/api/specs/authorization/PostBringAuthSpec.java
@@ -33,12 +33,12 @@ public class PostBringAuthSpec {
             .expectStatusCode(401)
             .build();
 
-    public static RequestSpecification unsuccessfulDeleteListSpec = with()
-            .spec(baseRequestSpec)
-            .log().uri()
-            .log().method()
-            .log().headers();
-
+    //public static RequestSpecification unsuccessfulDeleteListSpec = with()
+    //        .spec(baseRequestSpec)
+    //        .log().uri()
+    //        .log().method()
+    //        .log().headers();
+    //В продукте не нашлось DELETE запросов.
 }
 
 
