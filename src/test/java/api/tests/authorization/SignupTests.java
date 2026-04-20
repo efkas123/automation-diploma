f5c68d147b794692f05af6b5503daac5c8e39395
Filipp Kotov <contact@filippkotov.ru>
2026-04-20 14:52:42 +0300

    Updated signupAttemptTest to signupWithoutCaptchaTokenAttemptTest

--- src/test/java/api/tests/authorization/SignupTests.java
+++ src/test/java/api/tests/authorization/SignupTests.java
@@ -1,14 +1,19 @@
 package api.tests.authorization;
 
+import config.APIConfig;
+import config.ConfigProvider;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
+import static api.specs.BaseSpec.baseRequestSpec;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 @DisplayName("Работа регистрации через API.")
 @Epic("Регистрация")
@@ -16,19 +21,25 @@ import static io.restassured.RestAssured.given;
 @Story("Регистрация пользователя.")
 public class SignupTests {
 
+    APIConfig api = ConfigProvider.api();
+
     @Test
-    @DisplayName("Попытка регистрации пользователя с обходом капчи")
+    @DisplayName("Попытка регистрации пользователя без указания капча токена.")
     @Tags({
             @Tag("API"),
             @Tag("Негативный"),
             @Tag("Регистрация")
     })
-    void signupAttemptTest(){
+    void signupWithoutCaptchTokenAttemptTest(){
         given()
+                .spec(baseRequestSpec)
                 .when()
-                .post("https://api.getbring.com/rest/v2/bringauth/signup")
+                .post(api.authPath() + "/signup")
                 .then()
                 .statusCode(400)
+                .contentType(containsString("text/html"))
+                .body(containsString("HTTP Status 400"))
+                .body(containsString("Bad Request"))
                 .log().all();
     }
 
