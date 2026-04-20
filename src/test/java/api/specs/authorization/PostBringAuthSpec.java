d2b9b332b59521f02aa46ae3aa400e58a08a5f4b
Filipp Kotov <contact@filippkotov.ru>
2026-04-20 15:13:56 +0300

    Updated SuccessfullLoginAttemptTest200 and unsuccessfulBadCredentialsLogin401Test with body assertions

--- src/test/java/api/specs/authorization/PostBringAuthSpec.java
+++ src/test/java/api/specs/authorization/PostBringAuthSpec.java
@@ -10,6 +10,7 @@ import static api.specs.BaseSpec.baseResponseSpec;
 import static helpers.CustomListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.*;
+import static org.hamcrest.Matchers.*;
 
 public class PostBringAuthSpec {
 
@@ -23,11 +24,18 @@ public class PostBringAuthSpec {
     public static ResponseSpecification postBringAuthResponseSpec = new ResponseSpecBuilder()
             .log(ALL)
             .expectStatusCode(200)
+            .expectBody("$", hasKey("uuid"))
+            .expectBody("$", hasKey("name"))
+            .expectBody("email", not(emptyOrNullString()))
             .build();
 
     public static ResponseSpecification unsuccessfulBadCredentialLoginSpec = new ResponseSpecBuilder()
             .addResponseSpecification(baseResponseSpec)
             .expectStatusCode(401)
+            .expectBody("message", equalTo("email password combination not existing"))
+            .expectBody("error", equalTo("unauthorized"))
+            .expectBody("error_description", equalTo("Not authorized to access this resource"))
+            .log(ALL)
             .build();
 
 }
